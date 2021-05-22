39e09e21cac6131d1c1a737005e4d67d894efaac
Md. Mahin Hossain <intern67.it@akij.net>
2021-05-22 17:50:04 +0600

    design

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import NewsList from "./NewsList";
 import Pagination from "./Pagination";
 import { newscatagory } from "./Newscatagory";
 import { newsData } from "./NewsData";
+import Loading from "./Loading";
+
 function App() {
   return (
     <div className=" app container p-5">
       <div class="row">
         <div className="col-sm-6 offset-md-3">
           <Headers newscatagory={newscatagory.business} />
+
+          <div className="d-flex">
+            <small className="text-black-50 text-dark">
+              About {0} result found
+            </small>
+            <small className="text-black-50 ml-auto">
+              page {1}of {110}{" "}
+            </small>
+          </div>
           <NewsList news={newsData} />
           <Pagination />
+          <Loading />
         </div>
       </div>
     </div>
---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+const Loading = () => (
+  <div className="d-flex align-items-center">
+    <h4>Loading......</h4>
+    <div
+      className="spinner-border text-danger ml-auto"
+      role="status"
+      aria-hidden="true"
+    ></div>
+  </div>
+);
+
+export default Loading;
